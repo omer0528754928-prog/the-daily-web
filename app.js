0510e3f96f67df858870790d6cbe84df6c061cd2
omer0528754928-prog <omer0528754928@gmail.com>
2026-09-16 05:35:24 +0300

    Add MongoDB and Express scaffold

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+process.loadEnvFile();
+
 const express = require('express');
+const path = require('node:path');
+const connectDB = require('./config/db');
 
 const app = express();
+
+connectDB();
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 module.exports = app;
